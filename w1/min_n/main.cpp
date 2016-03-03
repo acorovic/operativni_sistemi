31106cccf0beaa997c20cea915cebbf5f11a7eff
acorovic <124c41alexa@gmail.com>
2016-03-03 21:05:41 +0100

    dodat min_n

---
+++ w1/min_n/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> min_n(const vector<int>& v,	int n) {
+    vector<int> temp = v;
+    sort(temp.begin(), temp.end());
+
+    vector<int> pov;
+    for(int i = 0; i < n; i++)
+        pov.push_back(temp[i]);
+
+    return pov;
+}
+int main()
+{
+    int niz[] = {188,12,1,6,22,14};
+    vector<int> a(niz, niz + sizeof(niz)/sizeof(int));
+
+    vector<int> b = min_n(a,3);
+    for(auto it : b)
+        cout<<it<<endl;
+    return 0;
+}
