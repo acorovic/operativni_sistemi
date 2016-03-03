31a47e2cb93b94479a0c94dfc16924fbae324e94
acorovic <124c41alexa@gmail.com>
2016-03-03 20:33:30 +0100

    dodat maksimalni element

---
+++ w1/algoritmi_max/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout<< "unesite brojeve"<<endl;
+    vector<int> brojevi;
+    for(int i = 0; i < 5; i++) {
+        int br;
+        cin >> br;
+        brojevi.push_back(br);
+    }
+
+    cout << "maksimalni elemenat je "<< *max_element(brojevi.begin(), brojevi.end()) << endl;
+    return 0;
+}
